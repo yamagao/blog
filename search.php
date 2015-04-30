8421d7811395bccc65266850fa36ac099e7b4930
yamagao <yamagao@126.com>
2015-04-30 10:22:47 -0400

    change the format to same as category

--- search.php
+++ search.php
@@ -20,46 +20,22 @@
 		    <!-- BEGIN .eight columns -->
 		    <div class="eight columns">
 		
-				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+				<?php if (have_posts()) : while (have_posts()) : the_post();//echo wp_get_attachment_image( get_post_thumbnail_id(), 'full' );?>
 		        <?php $meta_box = get_post_custom($post->ID); $video = $meta_box['custom_meta_video'][0]; ?>
 		        
 		        <div <?php post_class('archive-result content holder'); ?> id="page-<?php the_ID(); ?>">
-		            
-			        <!-- BEGIN .article -->
-			        <div class="article">
-					
-						<h2 class="headline"><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-				            
-						<?php if ( $video ) : ?>
-				            <div class="feature-vid"><?php echo $video; ?></div>
-				        <?php else: ?>
-				            <?php if ( has_post_thumbnail()) { ?>
-				            	<a class="feature-img" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_post_thumbnail( 'feature' ); ?></a>
-				            <?php } else { } ?>
-				        <?php endif; ?>
-				
-				        <?php the_excerpt(); ?>
-				        <div class="clear"></div>   
-					
-					<!-- END .article -->
-					</div>
-					
-					<div class="postmeta">
+					<?php //echo wp_get_attachment_image( get_post_thumbnail_id(), 'full' );?>
+		        	<?php if(!get_post_format()) { get_template_part('format', 'standard'); } else { get_template_part('format', get_post_format()); } ?>
+		        	<div class="postmeta">
 						<p class="left"><i class="icon-reorder"></i> &nbsp;<?php _e("Category:", 'organicthemes'); ?> <?php the_category(', '); ?></p> 
 						<?php if ( has_tag() ) { ?>
 							<p class="right"><i class="icon-tags"></i> &nbsp;<?php _e("Tags:", 'organicthemes'); ?> <?php the_tags(''); ?></p>
 						<?php } else { } ?>
 					</div>
-				
-				</div>
+		        </div>
 		
 				<?php endwhile; else: ?>         
-		        	<div class="post content holder">
-		        		<div class="article">
-		        			<h2 class="headline"><?php _e("No Results Found", 'organicthemes'); ?></h2>
-		        			<p><?php _e("Sorry, no posts matched your criteria.", 'organicthemes'); ?></p>
-		        		</div>
-		        	</div>
+		        <p><?php _e("Sorry, no posts matched your criteria.", 'organicthemes'); ?></p>
 				<?php endif; ?>
 		        
 		        <div class="pagination">
@@ -72,62 +48,33 @@
 		    <div class="four columns">
 		        <?php get_sidebar(); ?> 
 		    </div><!-- .four columns -->
-	    
-	    <?php else : ?>
-	    
-	    	<!-- BEGIN .twelve columns -->
-    	    <div class="twelve columns">
-    	
-    			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-    	        <?php $meta_box = get_post_custom($post->ID); $video = $meta_box['custom_meta_video'][0]; ?>
-    	        
-    	        <div <?php post_class('archive-result content holder'); ?> id="page-<?php the_ID(); ?>">
-    	            
-    		        <!-- BEGIN .article -->
-    		        <div class="article">
-    				
-    					<h2 class="headline"><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-    			            
-    					<?php if ( $video ) : ?>
-    			            <div class="feature-vid"><?php echo $video; ?></div>
-    			        <?php else: ?>
-    			        	<?php if ( has_post_thumbnail()) { ?>
-    			            	<a class="feature-img" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_post_thumbnail( 'feature' ); ?></a>
-    			            <?php } else { } ?>
-    			        <?php endif; ?>
-    			
-    			        <?php the_excerpt(); ?>
-    			        <div class="clear"></div>   
-    				
-    				<!-- END .article -->
-    				</div>
-    				
-    				<div class="postmeta">
-    					<p class="left"><i class="icon-reorder"></i> &nbsp;<?php _e("Category:", 'organicthemes'); ?> <?php the_category(', '); ?></p> 
-    					<?php if ( has_tag() ) { ?>
-    						<p class="right"><i class="icon-tags"></i> &nbsp;<?php _e("Tags:", 'organicthemes'); ?> <?php the_tags(''); ?></p>
-    					<?php } else { } ?>
-    				</div>
-    			
-    			</div>
-    	
-    			<?php endwhile; else: ?>         
-	    	        <div class="post content holder">
-	    	        	<div class="article">
-	    	        		<h2 class="headline"><?php _e("No Results Found", 'organicthemes'); ?></h2>
-	    	        		<p><?php _e("Sorry, no posts matched your criteria.", 'organicthemes'); ?></p>
-	    	        	</div>
-	    	        </div>
-    			<?php endif; ?>
-    	        
-    	        <div class="pagination">
-    	        	<?php if (function_exists("number_paginate")) { number_paginate(); } ?>
-    	        </div>
-    	
-    		<!-- END .twelve columns -->
-    	    </div>
-    	    
-    	<?php endif; ?>
+		    
+		<?php else : ?>
+		
+			<!-- BEGIN .twelve columns -->
+		    <div class="twelve columns">
+		
+				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+		        
+		        <div <?php post_class('archive-result content holder'); ?> id="page-<?php the_ID(); ?>">
+		        
+		        	<?php if(!get_post_format()) { get_template_part('format', 'standard'); } else { get_template_part('format', get_post_format()); } ?>
+		        
+		        </div>
+		
+				<?php endwhile; else: ?>         
+		        <p><?php _e("Sorry, no posts matched your criteria.", 'organicthemes'); ?></p>
+				<?php endif; ?>
+		        
+		        <div class="pagination">
+		        	<?php if (function_exists("number_paginate")) { number_paginate(); } ?>
+		        </div>
+		
+			<!-- END .twelve columns -->
+		    </div>
+			    
+		<?php endif; ?>
+		
 	
 	<!-- END .row -->
 	</div>
